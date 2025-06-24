b22d8b04f7fe42b8d6ccb1f447e49d1c35a9ff32
jeanitaz <jeanzeykoharcode@gmail.com>
2025-06-24 10:27:15 -0500

    moviles2

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import NavegadorBottom from './navigations/BottomTab';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavegadorBottom/>
   );
 }
 
@@ -17,4 +15,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
+ 
